98b3c65ddb809c3c88858533b6e17a9b9409d49d
Izotov <andrey.iztv@gmail.com>
2025-04-21 14:07:36 +0700

    Внесены небольшие изменения.

--- mwlab/__init__.py
+++ mwlab/__init__.py
@@ -1,5 +1,5 @@
 # mwlab/__init__.py
 from .io.touchstone import TouchstoneData
-from .datasets import TouchstoneDataset
+from .datasets.touchstone_dataset import TouchstoneDataset
 from .utils.analysis import TouchstoneDatasetAnalyzer
 
