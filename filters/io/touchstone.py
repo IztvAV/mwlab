36321a24f823f3f5c135b252f36791729c805bc1
Ilya <burlakov.ily@yandex.ru>
2025-04-30 13:53:56 +0700

    Добавил функцию для копирования класса Network

--- filters/io/touchstone.py
+++ filters/io/touchstone.py
@@ -1,12 +1,17 @@
 from mwlab import TouchstoneData
 import pathlib
-from typing import Union
+from typing import Union, Any, Dict
 from filters.filter.mwfilter import MWFilter
 
 
 class TouchstoneMWFilterData(TouchstoneData):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    @staticmethod
+    def _params_from_file(path: pathlib.Path) -> Dict[str, Any]:
+        net = MWFilter(str(path))
+        return {"f0": net.f0, "bw": net.bw, "Q": net.Q, "matrix": net.coupling_matrix.factors}
 
     @classmethod
     def load(cls, path: Union[str, pathlib.Path]) -> "TouchstoneData":
