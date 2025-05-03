9d8d95d18a73e9a0193a6ab90f19da55d7e803a8
Ilya <burlakov.ily@yandex.ru>
2025-05-03 13:31:23 +0700

    Начал реализовывать генератор датасетов. Добавил класс для сэмплеров.

--- filters/datasets/theoretical_dataset_generator.py
+++ filters/datasets/theoretical_dataset_generator.py
@@ -1,13 +1,55 @@
 import os
 import numpy as np
 from filters import MWFilter
+from ..utils import Sampler
+from dataclasses import dataclass
+
+
+@dataclass
+class CMTheoreticalDatasetGeneratorSamplers:
+    cm_shifts: Sampler
+    ps_shifts: Sampler
 
 
 class CMTheoreticalDatasetGenerator:
     def __init__(self,
-                 path_to_save_dataset: str,
+                 path_to_save_dataset: str, # Путь к директориям с датасетами
                  origin_filter: MWFilter,
-                 cm_shifts: np.array,
-                 ps_shifts: np.array,
+                 samplers: CMTheoreticalDatasetGeneratorSamplers
                  ):
-        pass
+        self._samplers = samplers
+        self._origin_filter = origin_filter
+        self._path_to_save_dataset = path_to_save_dataset
+
+    """TODO: Пока поддержана только свернутая топология. Сдвиги формируются как в старом методе"""
+    def _get_cm_shift(self):
+        m = self._origin_filter.coupling_matrix.matrix
+        for idx in range(len(self._samplers.cm_shifts)):
+            delta = self._samplers.cm_shifts[idx]
+            for (k, (i, j)) in enumerate(self._origin_filter.coupling_matrix.links):
+                if i == j:
+                    m[i][j] += delta[0]
+                elif j == (i + 1):
+                    m[i][j] += delta[1]
+                else:
+                    m[i][j] += delta[2]
+            yield m
+
+    def _get_ps_shift(self):
+        for i in range(len(self._samplers.ps_shifts)):
+            yield self._samplers.ps_shifts[i]
+
+    def generate(self):
+        if len(self._samplers.ps_shifts) != len(self._samplers.cm_shifts):
+            raise ValueError(f"Размер сэмплера с фазовыми сдвигами (ps_shifts): {len(self._samplers.ps_shifts)}"
+                             f" должен равняться размеру сэмплера со сдвигами элементов матрицы связи (cm_shifts): "
+                             f"{len(self._samplers.cm_shifts)}")
+        size = len(self._samplers.cm_shifts)
+        for _ in range(size):
+            new_matrix = self._get_cm_shift()
+            ps_shifts = self._get_ps_shift()
+            S = MWFilter.response_from_coupling_matrix(M=new_matrix, f0=self._origin_filter.f0,
+                                                       FBW=self._origin_filter.fbw, Q=self._origin_filter.Q,
+                                                       frange=self._origin_filter.f, Pss=ps_shifts)
+            # TODO: сделать сохранение в датасет S-параметров и матрицы связи
+
---
+++ filters/utils/samplers.py
@@ -0,0 +1,38 @@
+import enum
+import numpy as np
+
+
+class SamplerTypes(enum.Enum):
+    SAMPLER_UNIFORM = 0,
+    SAMPLER_FACTORIAL = 1,
+    SAMPLER_RANDOM = 2,
+    SAMPLER_LATIN_HYPERCUBE = 3,
+
+
+class Sampler:
+    def __init__(self, type: SamplerTypes, space: np.array):
+        self._type = type
+        self._space = space
+
+    @classmethod
+    def uniform(cls, start, stop, step):
+        space = np.linspace(start, stop, step)
+        return cls(type=SamplerTypes.SAMPLER_UNIFORM, space=space)
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def range(self):
+        return self._space
+
+    def __str__(self):
+        return f"Sampler with type: {self._type.name}"
+
+    def __len__(self):
+        return len(self._space)
+
+    def __getitem__(self, idx):
+        return self._space[idx]
+
