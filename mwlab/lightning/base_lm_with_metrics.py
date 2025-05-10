098fb4c9bf69f7e9a02a9f6867ece9b086747eae
Izotov <andrey.iztv@gmail.com>
2025-05-10 17:15:01 +0700

    Внесены изменения

--- mwlab/lightning/base_lm_with_metrics.py
+++ mwlab/lightning/base_lm_with_metrics.py
@@ -124,14 +124,18 @@ class BaseLMWithMetrics(BaseLModule):
     #                        validation / test loop
     # ======================================================================
     def validation_step(self, batch, batch_idx):
-        x, y, _ = self._split_batch(batch)        # метод унаследован
+        x, y, _ = self._split_batch(batch)
         preds = self(x)
         y_t = self._prepare_targets(y)
 
         loss = self.loss_fn(preds, y_t)
         self.log("val_loss", loss, on_epoch=True, prog_bar=True, batch_size=x.size(0))
 
-        metric_dict = self.val_metrics(preds, y_t)
+        # 💡 Flatten для совместимости с метриками
+        preds_flat = preds.view(preds.size(0), -1)
+        y_flat = y_t.view(y_t.size(0), -1)
+
+        metric_dict = self.val_metrics(preds_flat, y_flat)
         self.log_dict(metric_dict, on_epoch=True, prog_bar=True, batch_size=x.size(0))
         return loss
 
@@ -143,7 +147,11 @@ class BaseLMWithMetrics(BaseLModule):
         loss = self.loss_fn(preds, y_t)
         self.log("test_loss", loss, on_epoch=True, prog_bar=True, batch_size=x.size(0))
 
-        metric_dict = self.test_metrics(preds, y_t)
+        # 💡 Flatten для метрик
+        preds_flat = preds.view(preds.size(0), -1)
+        y_flat = y_t.view(y_t.size(0), -1)
+
+        metric_dict = self.test_metrics(preds_flat, y_flat)
         self.log_dict(metric_dict, on_epoch=True, prog_bar=True, batch_size=x.size(0))
         return loss
 
