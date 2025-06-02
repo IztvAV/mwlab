768f29531ecd7bc3395dec6042af6c49427ebeb9
Ilya <burlakov.ily@yandex.ru>
2025-06-02 18:19:51 +0700

    Добавил skip-connection для mlp корректора.

--- filters/codecs/mwfilter_touchstone_codec.py
+++ filters/codecs/mwfilter_touchstone_codec.py
@@ -5,6 +5,14 @@ class MWFilterTouchstoneCodec(TouchstoneCodec):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # ----------------------------------------------------------------- factory
+    @classmethod
+    def from_dataset(cls, keys_for_analysis, *args, **kwargs) -> "MWFilterTouchstoneCodec":
+        obj = super().from_dataset(*args, **kwargs)
+        # Удаляем ненужные индексы
+        obj.x_keys = keys_for_analysis
+        return obj
+
     def exclude_keys(self, keys_to_exclude=["f0", "bw", "N", "Q"]):  # Q исключаю сейчас для простоты
         # Исключаем из анализа ненужные x-параметры
         self.x_keys = list(filter(lambda x: x not in keys_to_exclude, self.x_keys))
--- filters/mwfilter_optim/bfgs.py
+++ filters/mwfilter_optim/bfgs.py
@@ -72,6 +72,7 @@ from filters.filter import MWFilter
 from filters.filter.couplilng_matrix import CouplingMatrix
 from scipy.optimize import minimize
 from filters.mwfilter_optim.base import FastMN2toSParamCalculation
+from filters.datasets.theoretical_dataset_generator import DatasetMWFilter
 
 
 def create_bounds(origin_matrix: CouplingMatrix):
@@ -80,13 +81,13 @@ def create_bounds(origin_matrix: CouplingMatrix):
 
     for (i, j) in origin_matrix.links:
         if i == j:
-            Mmin[i][j] -= 0.99*Mmin[i][j]
-            Mmax[i][j] += 0.2*Mmax[i][j]
+            Mmin[i][j] -= 0.5*Mmin[i][j]
+            Mmax[i][j] += 0.5*Mmax[i][j]
         elif j == (i + 1):
-            Mmin[i][j] -= 0.3*Mmin[i][j]
-            Mmin[j][i] -= 0.3*Mmin[j][i]
-            Mmax[i][j] += 0.3*Mmax[i][j]
-            Mmax[j][i] += 0.3*Mmax[j][i]
+            Mmin[i][j] -= 0.5*Mmin[i][j]
+            Mmin[j][i] -= 0.5*Mmin[j][i]
+            Mmax[i][j] += 0.5*Mmax[i][j]
+            Mmax[j][i] += 0.5*Mmax[j][i]
         else:
             Mmin[i][j] -= 0.5*Mmin[i][j]
             Mmin[j][i] -= 0.5*Mmin[j][i]
@@ -96,7 +97,7 @@ def create_bounds(origin_matrix: CouplingMatrix):
 
 
 
-def optimize_cm(pred_filter: MWFilter, orig_filter: MWFilter):
+def optimize_cm(pred_filter: DatasetMWFilter, orig_filter: DatasetMWFilter):
     def cost_with_grad(x_np, *args):
         """Функция стоимости + градиент для scipy"""
         fast_calc, orig_filter, s11_origin_db, s21_origin_db, links, matrix_order = args
@@ -105,8 +106,8 @@ def optimize_cm(pred_filter: MWFilter, orig_filter: MWFilter):
         M = CouplingMatrix.from_factors(x, links, matrix_order)
         _, s11_pred, s21_pred = fast_calc.RespM2(M)
 
-        loss = torch.sum(torch.abs(s11_origin_db - MWFilter.to_db(s11_pred))) + \
-               torch.sum(torch.abs(s21_origin_db - MWFilter.to_db(s21_pred)))
+        loss = torch.sum(torch.abs(s11_origin_db - DatasetMWFilter.to_db(s11_pred))) + \
+               torch.sum(torch.abs(s21_origin_db - DatasetMWFilter.to_db(s21_pred)))
 
         loss.backward()
         # print(f"{loss.item()}")
@@ -126,41 +127,32 @@ def optimize_cm(pred_filter: MWFilter, orig_filter: MWFilter):
     s21_origin = orig_filter.s[:, 1, 0]
 
     # Преобразуем в тензоры один раз
-    s11_origin_db = MWFilter.to_db(torch.tensor(s11_origin, dtype=torch.complex128))
-    s21_origin_db = MWFilter.to_db(torch.tensor(s21_origin, dtype=torch.complex128))
+    s11_origin_db = DatasetMWFilter.to_db(torch.tensor(s11_origin, dtype=torch.complex128))
+    s21_origin_db = DatasetMWFilter.to_db(torch.tensor(s21_origin, dtype=torch.complex128))
 
     # [(min(0.5*xi, 1*xi), max(0.5*xi, 1 * xi)) if xi != 0 else (-0.5, 0.5) for xi in x0]
 
     start_time = time.time()
-    result = minimize(
-        fun=cost_with_grad,
-        x0=x0,
-        method='BFGS',
-        jac=True,
-        args=(fast_calc, orig_filter, s11_origin_db, s21_origin_db, links, matrix_order),
-        options={'disp': True, 'maxiter': 100000, 'ftol': 1e-9, 'gtol': 1e-6, 'return_all': True}
-    )
-
     Mmin, Mmax = create_bounds(pred_filter.coupling_matrix)
     bounds = [(min(m_min, m_max), max(m_min, m_max)) for m_min, m_max in tuple(zip(Mmin.factors, Mmax.factors))]
     result = minimize(
         fun=cost_with_grad,
-        x0=result.x,
+        x0=x0,
         method='l-bfgs-b',
         jac=True,
         bounds=bounds,
         args=(fast_calc, orig_filter, s11_origin_db, s21_origin_db, links, matrix_order),
         options={'disp': True, 'maxiter': 100000, 'ftol': 1e-9, 'gtol': 1e-6}
     )
+    print(f"Cost after L-BFGS-B: {result.fun}")
 
     result = minimize(
         fun=cost_with_grad,
         x0=result.x,
-        method='bfgs',
+        method='BFGS',
         jac=True,
-        bounds=bounds,
         args=(fast_calc, orig_filter, s11_origin_db, s21_origin_db, links, matrix_order),
-        options={'disp': True, 'maxiter': 100000, 'ftol': 1e-9, 'gtol': 1e-6}
+        options={'disp': True, 'maxiter': 100000, 'ftol': 1e-9, 'gtol': 1e-6, 'return_all': True}
     )
 
     print(f"Cost after BFGS: {result.fun}")
