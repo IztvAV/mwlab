200654fa22a493b367503c35948e690551447850
Ilya <burlakov.ily@yandex.ru>
2025-06-19 11:52:59 +0700

    Добавил распределение Соболя и корректор ошибок на основе градиентного бустинга

--- configs.py
+++ configs.py
@@ -2,7 +2,7 @@ import os
 
 
 BATCH_SIZE = 64
-BASE_DATASET_SIZE = 100_000
+BASE_DATASET_SIZE = 34250
 FILTER_NAME = "EAMU4-KuIMUXT3-BPFC1"
 # FILTER_NAME = "SCYA501-KuIMUXT5-BPFC3"
 ENV_ORIGIN_DATA_PATH = os.path.join(os.getcwd(), "filters", "FilterData", FILTER_NAME, "origins_data")
